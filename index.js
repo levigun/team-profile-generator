8fe02095e2e00b2fde65619a2a31a1f48f226afc
levigun <t.levina31@gmail.com>
2022-02-18 13:48:19 +1100

    added code into index.js

---
+++ index.js
@@ -0,0 +1,120 @@
+// the required node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+const teamGenerator = require('./src/page-template');
+
+// the lib modules that has been created
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+
+// create an empty array for answer to the questions
+const newEmployee = [];
+
+// array of questions that will be asked to user
+const questions = async () => {
+    const answers = await inquirer
+    .prompt([
+        {
+            type:'input',
+            name:'name',
+            message:"What is your name?"
+        },
+        {
+            type:'input',
+            name:'id',
+            message:"What is your ID number?"
+        },
+        {
+            type:'input',
+            name:'email',
+            message:"What is your email?"
+        },
+        {
+            type:'list',
+            name:'role',
+            message:"What is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ])
+
+    // if engineer is chosen, these following questions will be added
+    if (answers.role === "Engineer") {
+        const engineerAns = await inquirer
+        .prompt([
+            {
+                type:'input',
+                name:'github',
+                message:"What is your Github username?"
+            },
+        ])
+            const newEngineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                engineerAns.github
+            );
+            newEmployee.push(newEngineer);
+
+        //  if manager is chosen, these following questions will be added  
+    } else if (answers.role === "Manager") {
+        const managerAns = await inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number",
+            }
+        ])
+        const newManager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+          );
+          newEmployee.push(newManager);
+
+    } else if (answers.role === "Intern") {
+        const internAns = await inquirer 
+        .prompt([
+            {
+                type: "input",
+                name: "school",
+                message: "Which education establishment did you go to?",
+            }
+        ])
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school,
+            );
+            newEmployee.push(newIntern); 
+        }
+};
+
+async function promptQuestions() {
+    await questions()
+
+    const addMemberAns = await inquirer
+        .prompt([
+            {
+                type:"list",
+                name:"addMember",
+                message: "What would you like to do next?",
+                choices: ["Add a new member", "Create team"]
+            }
+        ])
+
+
+        if (addMemberAns.addMember === 'Add a new member') {
+            return promptQuestions()
+        }
+        return createTeam();
+}
+
+promptQuestions();
+
+function createTeam() {
+    fs.writeFileSync("./dist/index.html", teamGenerator(newEmployee), "utf-8");
+}
